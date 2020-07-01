57ffa62fad78fe36722b9025277c7303e63b3863
DP <websiteservices@protonmail.com>
2020-07-01 13:10:58 -0400

    2nd attempt

---
+++ JS/project-quote-machine/src/quotes.js
@@ -0,0 +1,16 @@
+const QUOTES = [
+    {id: 1,
+    author: 'Ernest Hemingway',
+    quote: 'But man is not made for defeat. A man can be destroyed but not defeated.'},
+    {id: 2,
+    author: 'John Keats',
+    quote: 'I love you the more in that I believe you had liked me for my own sake and for nothing else..'},
+    {id: 3,
+    author: 'Franklin D. Roosevelt',
+    quote: 'When you reach the end of your rope, tie a knot in it and hang on..'},
+    {id: 4,
+    author: 'Heraclitus',
+    quote: 'There is nothing permanent except change.'}
+]
+
+export default QUOTES
